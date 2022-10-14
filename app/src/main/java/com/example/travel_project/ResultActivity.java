2b00248c0bc0db62835d989133585d7fa59b4d93
Lamvince <vince.lam120@gmail.com>
2022-10-13 23:32:14 -0700

    activity_results.xml now displays a list using values from an ArrayList

--- app/src/main/java/com/example/travel_project/ResultActivity.java
+++ app/src/main/java/com/example/travel_project/ResultActivity.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
@@ -19,10 +20,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ResultActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        ListView list = findViewById(R.id.resultsList);
+        ArrayList<String> arrayList = new ArrayList<String>();
+        arrayList.add("Event Result One");
+        arrayList.add("Event Result Two");
+        arrayList.add("Event Result Three");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1, arrayList );
+
+        list.setAdapter(arrayAdapter);
     }
 }
