d1a7ed761b86f9f52d508bfc673db913e6cbdd37
Dongwan Kang <dkang48@my.bcit.ca>
2022-10-13 17:29:06 -0700

    Committing activity_info page

---
+++ app/src/main/java/com/example/travel_project/InfoPage.java
@@ -0,0 +1,27 @@
+package com.example.travel_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class InfoPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+    }
+}
