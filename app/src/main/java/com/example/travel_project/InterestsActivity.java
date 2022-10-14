1a373ff86c443c6892844a05ceb5c82a7c281450
valerie <valerietan000@gmail.com>
2022-10-13 17:33:58 -0700

    added interests java + layout

---
+++ app/src/main/java/com/example/travel_project/InterestsActivity.java
@@ -0,0 +1,21 @@
+package com.example.travel_project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InterestsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interests);
+    }
+
+    Button toActivityInfo = findViewById(R.id.btnToInfo);
+        toActivityInfo.setOnClickListener(view -> {
+        Intent intent = new Intent(this, activityInfo.class);
+        startActivity(intent);
+    });
+}
