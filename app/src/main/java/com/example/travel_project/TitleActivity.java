8a5d4cb743893cc56fb0243cf06cc0463d89ca10
Dongwan Kang <dkang48@my.bcit.ca>
2022-11-09 14:22:47 -0800

    login and signup complete

--- app/src/main/java/com/example/travel_project/TitleActivity.java
+++ app/src/main/java/com/example/travel_project/TitleActivity.java
@@ -2,21 +2,78 @@ package com.example.travel_project;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class TitleActivity extends AppCompatActivity {
+
+    private EditText emailTextView, passwordTextView;
+    private Button Btn;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
 
-        Button toActivityCountry = findViewById(R.id.btnToCountry);
-        toActivityCountry.setOnClickListener(view -> {
-            Intent intent = new Intent(this, CountryActivity.class);
-            startActivity(intent);
+        mAuth = FirebaseAuth.getInstance();
+        emailTextView = findViewById(R.id.userName);
+        passwordTextView = findViewById(R.id.username);
+        Btn = findViewById(R.id.btnToCountry);
+        Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginUserAccount();
+            }
+        });
+
+        Button toSignUp = findViewById(R.id.toSignUp);
+        toSignUp.setOnClickListener(view -> {
+            Intent intent2 = new Intent(this, SignupActivity.class);
+            startActivity(intent2);
+        });
+    }
+
+    private void loginUserAccount() {
+        String email, password;
+        email = emailTextView.getText().toString();
+        password = passwordTextView.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(),
+                            "Please enter email!!",
+                            Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(),
+                            "Please enter password!!",
+                            Toast.LENGTH_LONG).show();
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Login Successful!", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(TitleActivity.this, CountryActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Login Failed!", Toast.LENGTH_LONG).show();
+                }
+            }
         });
     }
 }
